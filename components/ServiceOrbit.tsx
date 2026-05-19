3b90d0f8da8aed1ac1b6eb8254225756ff00cfde
shahzaibkhatri918-stack <shahzaibkhatri918@gmail.com>
2026-05-20 01:27:02 +0500

    Update ServiceOrbit.tsx

--- components/ServiceOrbit.tsx
+++ components/ServiceOrbit.tsx
@@ -2,18 +2,52 @@ import Link from "next/link";
 import { orbitServices } from "../data/site";
 
 export default function ServiceOrbit() {
-  const repeated = [...orbitServices, ...orbitServices, ...orbitServices, ...orbitServices];
+  const repeated = [
+    ...orbitServices,
+    ...orbitServices,
+    ...orbitServices,
+    ...orbitServices,
+  ];
 
   return (
     <div className="orbitBox fade-up">
       <div className="orbitTrack trackOne">
-        {repeated.map((item, i) => <Link href="/services" className="orbitChip" key={"a"+i}><span></span>{item}</Link>)}
+        {repeated.map((item, i) => (
+          <Link
+            href="/services"
+            className="orbitChip"
+            key={`a-${i}`}
+          >
+            <span></span>
+            {item}
+          </Link>
+        ))}
       </div>
+
       <div className="orbitTrack trackTwo">
-        {repeated.map((item, i) => <Link href="/services" className="orbitChip" key={"b"+i}><span></span>{item}</Link>)}
+        {repeated.map((item, i) => (
+          <Link
+            href="/services"
+            className="orbitChip"
+            key={`b-${i}`}
+          >
+            <span></span>
+            {item}
+          </Link>
+        ))}
       </div>
+
       <div className="orbitTrack trackThree">
-        {repeated.map((item, i) => <Link href="/services" className="orbitChip" key={"c"+i}><span></span>{item}</Link>)}
+        {repeated.map((item, i) => (
+          <Link
+            href="/services"
+            className="orbitChip"
+            key={`c-${i}`}
+          >
+            <span></span>
+            {item}
+          </Link>
+        ))}
       </div>
     </div>
   );
