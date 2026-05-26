11e8e77bdd591d72d81d491e6d91bd55e2e9db48
shahzaibkhatri918-stack <shahzaibkhatri918@gmail.com>
2026-05-27 00:30:36 +0500

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -121,7 +121,25 @@ export default function PortfolioPage() {
         <section className="section fade-up">
           <PortfolioFilter />
         </section>
+        {/* CTA SECTION */}
+<section className="ctaBanner fade-up">
+  <div className="ctaBannerContent">
 
+    <div className="ctaText">
+      <span className="pill">Let’s Work Together</span>
+
+      <h2>
+        Ready To Turn Your Ideas Into{" "}
+        <span className="yellow">High-Converting Videos?</span>
+      </h2>
+    </div>
+
+    <a href="#project-form" className="ctaBigBtn">
+      Start Your Project
+    </a>
+
+  </div>
+</section>
         <section id="project-form" className="section fade-up">
           <div
             style={{
